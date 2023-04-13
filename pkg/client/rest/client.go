4cee04d33310dd34a08dc8984297defdf22199f6
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2023-04-13 11:50:42 +0300

    [STR-2146] geoname rest client

--- pkg/client/rest/client.go
+++ pkg/client/rest/client.go
@@ -62,3 +62,33 @@ func (c *Client) City(ctx context.Context, address string) (*entity.City, error)
 func (c *Client) CityLite(ctx context.Context, address, lang string) (*entity.CityLite, error) {
 	return get[entity.CityLite](ctx, c.client, "city-lite/"+address, nil)
 }
+
+func getMany[T any](ctx context.Context, client *resty.Client, path string, query url.Values) ([]*T, error) {
+	request := client.R().SetContext(ctx)
+	if query != nil {
+		request = request.SetQueryParamsFromValues(query)
+	}
+	var obj []*T
+	resp, err := request.Get(path)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(resp.Body(), &obj)
+	if err != nil {
+		return nil, err
+	}
+	return obj, nil
+}
+
+func (c *Client) GeoNameCountry(ctx context.Context) ([]*entity.GeoNameCountry, error) {
+	return getMany[entity.GeoNameCountry](ctx, c.client, "geoname/country", nil)
+}
+
+func (c *Client) GeoNameSubdivision(ctx context.Context) ([]*entity.AdminSubdivision, error) {
+	return getMany[entity.AdminSubdivision](ctx, c.client, "geoname/subdivision", nil)
+}
+
+func (c *Client) GeoNameCity(ctx context.Context) ([]*entity.Geoname, error) {
+	return getMany[entity.Geoname](ctx, c.client, "geoname/city", nil)
+}
