7877770e45fae6d9bc503e681a0f81f5f897cc52
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2024-01-11 17:43:30 +0300

    [STR-2675] added custom maxmind database struct

---
+++ pkg/repository/custom_maxmind_database.go
@@ -0,0 +1,82 @@
+package repository
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net"
+
+	"github.com/maxmind/mmdbwriter"
+	"github.com/maxmind/mmdbwriter/inserter"
+	"github.com/maxmind/mmdbwriter/mmdbtype"
+	"github.com/oschwald/maxminddb-golang"
+)
+
+type MMDBRecord struct {
+	Network *net.IPNet
+	Data    mmdbtype.Map
+}
+
+type MMDBRecordReader interface {
+	ReadMMDBRecord() (MMDBRecord, error)
+}
+
+type CustomMaxMindDB struct {
+	tree  *mmdbwriter.Tree
+	dbRaw []byte
+	db    *maxminddb.Reader
+}
+
+func NewCustomMaxMindDB(reader MMDBRecordReader) (*CustomMaxMindDB, error) {
+	tree, err := mmdbwriter.New(mmdbwriter.Options{IncludeReservedNetworks: true})
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		rec, err := reader.ReadMMDBRecord()
+		if err != nil {
+			break
+		}
+		if err := tree.InsertFunc(rec.Network, inserter.TopLevelMergeWith(rec.Data)); err != nil {
+			return nil, err
+		}
+	}
+
+	if err != nil && !errors.Is(err, io.EOF) {
+		return nil, err
+	}
+	err = nil
+
+	var buf bytes.Buffer
+	if _, err := tree.WriteTo(&buf); err != nil {
+		return nil, err
+	}
+	dbRaw := buf.Bytes()
+
+	db, err := maxminddb.FromBytes(buf.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	return &CustomMaxMindDB{
+		tree:  tree,
+		dbRaw: dbRaw,
+		db:    db,
+	}, nil
+}
+
+func (db *CustomMaxMindDB) Available() bool {
+	return true
+}
+
+func (db *CustomMaxMindDB) Lookup(ip net.IP, result interface{}) error {
+	return db.db.Lookup(ip, result)
+}
+
+func (db *CustomMaxMindDB) RawData() (io.Reader, error) {
+	return bytes.NewReader(db.dbRaw), nil
+}
+
+func (db *CustomMaxMindDB) MetaData() (*maxminddb.Metadata, error) {
+	return &db.db.Metadata, nil
+}
